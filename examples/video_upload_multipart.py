41558177d1fb7e0b785ab276de77118439923397
Jesse Gumz <jesse@jwplayer.com>
2017-01-25 14:44:56 -0500

    incorporate PR feedback

--- examples/video_upload_multipart.py
+++ examples/video_upload_multipart.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import datetime
-from jwplatform import Client
-from jwplatform.errors import JWPlatformError
 import logging
 import os
 import requests
-import json
-import sys
-import argparse
+
+from jwplatform import Client
+from jwplatform.errors import JWPlatformError
+
 
 logging.basicConfig(level=logging.INFO)
 
-JW_API_KEY = os.environ.get('JWP_API_KEY')
-JW_API_SECRET = os.environ.get('JWP_API_SECRET')
+JW_API_KEY = os.environ.get('JW_API_KEY')
+JW_API_SECRET = os.environ.get('JW_API_SECRET')
 
 
 class MultipartUploader(object):
     # set default buffer size to 10 MB
-    BUFFER_SIZE = 1000000
+    BYTES_TO_BUFFER = 10000000
 
     def __init__(self, video_file_path):
         self._file_path = video_file_path
         self._file_size = os.stat(video_file_path).st_size
-        self._file_name = os.path.split(video_file_path)[1]
+        self._file_name = os.path.basename(video_file_path)
         self._setup()
 
     def _setup(self):
@@ -51,21 +49,19 @@ class MultipartUploader(object):
             self._offset = 0
 
         except JWPlatformError as e:
-            logging.exception('An error occurred during the uploader setup. Check that your API keys are properly '
-                              'set up in your environment, and ensure that the video file path exists.')
+            logging.error('An error occurred during the uploader setup. Check that your API keys are properly '
+                          'set up in your environment, and ensure that the video file path exists.')
             raise
 
     def _upload_chunk(self, chunk):
         """
         Handles the POST request needed to upload a single portion of the video file.
         Serves as a helper method for upload().
-
         The offset used to determine where a chunk begins and ends is updated in the course of
         this method's execution.
 
         :param chunk: <byte[]> the raw bytes of data from the video file
         """
-
         begin_chunk = self._offset
         # the last byte in the range is INCLUSIVE, hence the -1
         end_chunk = begin_chunk + len(chunk) - 1
@@ -76,11 +72,8 @@ class MultipartUploader(object):
         self._headers.update({'Content-Type': 'application/octet-stream'})
         self._headers.update({'Content-Length': str((end_chunk - begin_chunk) + 1)})
 
-        try:
-            response = requests.post(self._upload_url, params=self._query_parameters, headers=self._headers, data=chunk)
-            response.raise_for_status()
-        except requests.exceptions.RequestException:
-            raise
+        response = requests.post(self._upload_url, params=self._query_parameters, headers=self._headers, data=chunk)
+        response.raise_for_status()
 
         self._offset += len(chunk)
 
@@ -91,37 +84,30 @@ class MultipartUploader(object):
         """
         try:
             with open(self._file_path, 'rb') as file_to_upload:
-                chunk = file_to_upload.read(self.BUFFER_SIZE)
-                while len(chunk) > 0:
+                chunk = file_to_upload.read(self.BYTES_TO_BUFFER)
+                while True:
                     self._upload_chunk(chunk)
-                    chunk = file_to_upload.read(self.BUFFER_SIZE)
+                    chunk = file_to_upload.read(self.BYTES_TO_BUFFER)
+                    if len(chunk) <= 0:
+                        break
 
-        # catch any exceptions that bubbled up
-        except requests.exceptions.RequestException as e:
-            logging.exception("Error posting data, stopping upload...".format(e))
-            raise
+        # log any exceptions that bubbled up
+        except requests.exceptions.RequestException:
+            logging.exception('Error posting data, stopping upload...')
 
         except IOError:
             logging.exception('Could not read file to upload.')
-            raise
 
-        except Exception:
-            logging.exception('An error occurred during the multipart upload process')
-            raise
 
 if __name__ == "__main__":
 
+    import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--filepath', dest='filepath', type=str,
-                        help='Absolute path to video file')
+    parser.add_argument('--filepath', dest='filepath', type=str, help='Absolute path to video file')
     arguments = parser.parse_args()
     file_path = arguments.filepath
 
-    try:
-        uploader = MultipartUploader(video_file_path=file_path)
-        logging.info("Uploading {}".format(file_path))
-        uploader.run()
-        logging.info("Uploader finished successfully")
-
-    except Exception:
-        logging.error('Uploader failed')
+    uploader = MultipartUploader(video_file_path=file_path)
+    logging.info("Uploading {}".format(file_path))
+    uploader.run()
