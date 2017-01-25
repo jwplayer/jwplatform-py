cbe09253b8671449c42d7ae38cafc12968494541
Jesse Gumz <jesse@jwplayer.com>
2017-01-25 17:08:51 -0500

    Replace old example with new one

---
+++ examples/video_upload_multipart.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+import requests
+
+from jwplatform import Client
+from jwplatform.errors import JWPlatformError
+
+
+logging.basicConfig(level=logging.INFO)
+
+JW_API_KEY = os.environ.get('JW_API_KEY')
+JW_API_SECRET = os.environ.get('JW_API_SECRET')
+
+BYTES_TO_BUFFER = 10000000
+
+
+def run_upload(video_file_path):
+    """
+    Configures all of the needed upload_parameters and sets up all information pertinent
+    to the video to be uploaded.
+
+    :param video_file_path: <str> the absolute path to the video file
+    """
+    upload_parameters = dict()
+    upload_parameters['file_path'] = video_file_path
+    upload_parameters['file_size'] = os.stat(video_file_path).st_size
+    upload_parameters['file_name'] = os.path.basename(video_file_path)
+
+    try:
+        # Setup API client
+        jwplatform_client = Client(JW_API_KEY, JW_API_SECRET)
+
+        # Make /videos/create API call with multipart parameter specified
+        jwplatform_video_create_response = jwplatform_client.videos.create(
+            upload_method='multipart',
+            title=upload_parameters['file_name']
+        )
+
+        # Construct base url for upload
+        upload_parameters['upload_url'] = '{}://{}{}'.format(
+            jwplatform_video_create_response['link']['protocol'],
+            jwplatform_video_create_response['link']['address'],
+            jwplatform_video_create_response['link']['path']
+        )
+        logging.info('Upload URL to be used: {}'.format(upload_parameters['upload_url']))
+
+        upload_parameters['query_parameters'] = jwplatform_video_create_response['link']['query']
+        upload_parameters['query_parameters']['api_format'] = 'json'
+        upload_parameters['headers'] = {'X-Session-ID': jwplatform_video_create_response['session_id']}
+        # The chunk offset will be updated several times during the course of the upload
+        upload_parameters['chunk_offset'] = 0
+
+    except JWPlatformError:
+        logging.error('An error occurred during the uploader setup. Check that your API keys are properly '
+                      'set up in your environment, and ensure that the video file path exists.')
+        raise
+
+    # Perform the multipart upload
+    try:
+        with open(upload_parameters['file_path'], 'rb') as file_to_upload:
+            chunk = file_to_upload.read(BYTES_TO_BUFFER)
+            while True:
+                upload_chunk(chunk, upload_parameters)
+                chunk = file_to_upload.read(BYTES_TO_BUFFER)
+                if len(chunk) <= 0:
+                    break
+
+    # Log any exceptions that bubbled up
+    except requests.exceptions.RequestException:
+        logging.exception('Error posting data, stopping upload...')
+
+    except IOError:
+        logging.exception('Could not read file to upload.')
+
+
+def upload_chunk(chunk, upload_parameters):
+    """
+    Handles the POST request needed to upload a single portion of the video file.
+    Serves as a helper method for upload_by_multipart().
+    The offset used to determine where a chunk begins and ends is updated in the course of
+    this method's execution.
+
+    :param chunk: <byte[]> the raw bytes of data from the video file
+    :param upload_parameters: <dict> a collection of all pieces of info needed to upload the video
+    """
+    begin_chunk = upload_parameters['chunk_offset']
+    end_chunk = begin_chunk + len(chunk) - 1
+    file_size = upload_parameters['file_size']
+    filename = upload_parameters['file_size']
+
+    logging.info("begin_chunk / end_chunk = {} / {}".format(begin_chunk, end_chunk))
+    upload_parameters['headers'].update({'X-Content-Range': 'bytes {}-{}/{}'.format(begin_chunk, end_chunk, file_size)})
+    upload_parameters['headers'].update({'Content-Disposition': 'attachment; filename="{}"'.format(filename)})
+    upload_parameters['headers'].update({'Content-Type': 'application/octet-stream'})
+    upload_parameters['headers'].update({'Content-Length': str((end_chunk - begin_chunk) + 1)})
+
+    response = requests.post(
+        upload_parameters['upload_url'],
+        params=upload_parameters['query_parameters'],
+        headers=upload_parameters['headers'],
+        data=chunk
+    )
+    response.raise_for_status()
+
+    # Note that this places the next range one byte ahead of the old range, as desired
+    upload_parameters['chunk_offset'] = begin_chunk + len(chunk)
