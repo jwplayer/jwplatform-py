ff4454ba68ff9ae1699bb32da4dc37ae91ff0af0
Danny Gibson <dgibson@jwplayer.com>
2021-08-26 09:41:10 -0700

    Add optional captions data to CSV

--- examples/video_list_to_csv.py
+++ examples/video_list_to_csv.py
@@ -61,6 +61,10 @@ def make_csv(secret, site_id, path_to_csv=None, result_limit=1000, query_params=
         for video in next_videos:
             csv_video = video["metadata"]
             csv_video["id"] = video["id"]
+            csv_video['duration'] = video['duration']
+            captions = get_captions(api_client=jwplatform_client, site_id=site_id, media_id=video["id"])
+            csv_video['has_captions'] = True if len(captions) > 0 else False
+            csv_video['captions'] = captions
             videos.append(csv_video)
         page += 1
         logging.info("Accumulated {} videos.".format(len(videos)))
@@ -68,7 +72,7 @@ def make_csv(secret, site_id, path_to_csv=None, result_limit=1000, query_params=
             break
 
     # Section for writing video library to csv
-    desired_fields = ['id', 'title', 'description', 'tags', 'publish_start_date', 'permalink', 'duration', 'captions', 'custom_params', 'duration']
+    desired_fields = ['id', 'title', 'description', 'tags', 'publish_start_date', 'permalink', 'custom_params', 'duration', 'has_captions', 'captions']
     should_write_header = not os.path.isfile(path_to_csv)
     with open(path_to_csv, 'a+') as path_to_csv:
         # Only write columns to the csv which are specified above. Columns not specified are ignored.
@@ -76,3 +80,22 @@ def make_csv(secret, site_id, path_to_csv=None, result_limit=1000, query_params=
         if should_write_header:
             writer.writeheader()
         writer.writerows(videos)
+
+def get_captions(api_client, site_id, media_id):
+    captions_response = api_client.request(
+        method='GET',
+        path=f'https://api.jwplayer.com/v2/sites/{site_id}/media/{media_id}/text_tracks/'
+    )
+    captions = []
+    for text_track in captions_response.json_body['text_tracks']:
+        captions.append(
+            {
+                'created': text_track['created'],
+                'id': text_track['id'],
+                'metadata.label': text_track['metadata']['label'],
+                'metadata.srclang': text_track['metadata']['srclang'],
+                'status': text_track['status'],
+                'track_kind':text_track['track_kind']
+            }
+        )
+    return captions
